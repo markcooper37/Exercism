95edf714dd980bed836d7393e117ccb35ef10cdb
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-01-22 23:57:11 +0000

    Add Welcome To Tech Palace

---
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -0,0 +1,22 @@
+package techpalace
+
+import "strings"
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	starString := strings.Repeat("*", numStarsPerLine)
+    return starString + "\n" + welcomeMsg + "\n" + starString
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	oldMsg = strings.Replace(oldMsg, "*", "", -1)
+    oldMsg = strings.TrimSpace(oldMsg)
+    return oldMsg
+    
+}
