3ff3d28fe7e4d53c54ffddb760363841d439be36
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-01-23 00:32:39 +0000

    Add changes for Lasagna Master update

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,6 +1,5 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
 func PreparationTime(layers []string, timePerLayer int) int {
     if timePerLayer == 0 {
         return len(layers) * 2
@@ -8,31 +7,27 @@ func PreparationTime(layers []string, timePerLayer int) int {
 	return len(layers) * timePerLayer
 }
 
-// TODO: define the 'Quantities()' function
 func Quantities(layers []string) (int, float64) {
-    var noodles int = 0
-    var sauce float64 = 0
+    noodles := 0
+    sauce := 0.0
     for _, ingredient := range layers {
         if ingredient == "noodles" {
-            noodles = noodles + 50
+            noodles += 50
         } else if ingredient == "sauce" {
-        	sauce = sauce + 0.2
+        	sauce += 0.2
         }
     }
 	return noodles, sauce
 }
 
-// TODO: define the 'AddSecretIngredient()' function
 func AddSecretIngredient(friendsList []string, myList []string) []string {
-    lastItem := friendsList[len(friendsList) - 1]
-    newList := append(myList, lastItem)
-    return newList
+    myList[len(myList)-1] = friendsList[len(friendsList) - 1]
+    return myList
 }
 
-// TODO: define the 'ScaleRecipe()' function
 func ScaleRecipe(quantities []float64, numberOfPortions int) []float64 {
     var newQuantities = make([]float64, len(quantities))
-    var scalePortions float64 = float64(numberOfPortions) / 2
+    scalePortions := float64(numberOfPortions)/2
     for i := 0; i < len(quantities); i++ {
         newQuantities[i] = quantities[i] * scalePortions
     }
