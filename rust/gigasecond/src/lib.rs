07525ea2b4ea12679604159ecfdd0c7c0765d27a
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-13 23:45:55 +0000

    Add Gigasecond for Rust

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
+use time::Duration;
+
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.replace_time(time!(5:00))
+    start.saturating_add(Duration::seconds(1_000_000_000))
 }
