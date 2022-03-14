f5c775c39ed848b749098aedc7f11dfbdac8614d
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-14 17:29:12 +0000

    Add Linked List

---
+++ go/linked-list/linked_list.go
@@ -0,0 +1,105 @@
+package linkedlist
+
+import (
+	"errors"
+)
+
+type Node struct {
+	Val          interface{}
+	NextNode     *Node
+	PreviousNode *Node
+}
+
+type List struct {
+	head *Node
+	tail *Node
+	size int
+}
+
+var ErrEmptyList = errors.New("list has length zero")
+
+func NewList(args ...interface{}) *List {
+	newList := &List{}
+	for _, value := range args {
+		newList.PushBack(value)
+	}
+	return newList
+}
+
+func (n *Node) Next() *Node {
+	return n.NextNode
+}
+
+func (n *Node) Prev() *Node {
+	return n.PreviousNode
+}
+
+func (l *List) PushFront(v interface{}) {
+	l.head = &Node{Val: v, NextNode: l.head}
+	l.size++
+	if l.size == 1 {
+		l.tail = l.head
+	} else {
+		l.head.NextNode.PreviousNode = l.head
+	}
+}
+
+func (l *List) PushBack(v interface{}) {
+	l.tail = &Node{Val: v, PreviousNode: l.tail}
+	l.size++
+	if l.size == 1 {
+		l.head = l.tail
+	} else {
+		l.tail.PreviousNode.NextNode = l.tail
+	}
+}
+
+func (l *List) PopFront() (interface{}, error) {
+	if l.size == 0 {
+		return nil, ErrEmptyList
+	}
+	value := l.head.Val
+	if l.size == 1 {
+		l.head, l.tail = nil, nil
+	} else {
+		l.head = l.head.NextNode
+		l.head.PreviousNode = nil
+	}
+	l.size--
+	return value, nil
+}
+
+func (l *List) PopBack() (interface{}, error) {
+	if l.size == 0 {
+		return nil, ErrEmptyList
+	}
+	value := l.tail.Val
+	if l.size == 1 {
+		l.head, l.tail = nil, nil
+	} else {
+		l.tail = l.tail.PreviousNode
+		l.tail.NextNode = nil
+	}
+	l.size--
+	return value, nil
+}
+
+func (l *List) Reverse() {
+	if l.size == 0 {
+		return
+	}
+	l.tail, l.head = l.head, l.tail
+	currentNode := l.head
+	for currentNode != nil {
+		currentNode.NextNode, currentNode.PreviousNode = currentNode.PreviousNode, currentNode.NextNode
+		currentNode = currentNode.NextNode
+	}
+}
+
+func (l *List) First() *Node {
+	return l.head
+}
+
+func (l *List) Last() *Node {
+	return l.tail
+}
