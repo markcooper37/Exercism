e1d0d7d8b4392a55c833af7fab322d41a88427e0
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-14 10:47:53 +0000

    Add Grains for Rust

---
+++ rust/grains/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn square(s: u32) -> u64 {
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64")
+    }
+    let base: u64 = 2;
+    base.pow(s-1)
+
+}
+
+pub fn total() -> u64 {
+    let mut total: u64 = 0;
+    for i in 1..=64 {
+        total += square(i);
+    }
+    total
+}
