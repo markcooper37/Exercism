223ac23fa892cf6af2e84cc8911d897fb69265a3
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-15 00:20:17 +0000

    Add Sum of Multiples for Rust

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut sum: u32 = 0;
+    for i in 1..limit {
+        for factor in factors {
+            if *factor == 0 {
+                continue;
+            }
+            if i % factor == 0 {
+                sum += i;
+                break;
+            }
+        }
+    }
+    sum
+}
