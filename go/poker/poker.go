b76250b48c1051ac9941c350a1df841a0e962123
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-17 21:00:28 +0000

    Add rank-finding functionality

--- go/poker/poker.go
+++ go/poker/poker.go
@@ -2,13 +2,13 @@ package poker
 
 import (
 	"errors"
-	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type Card struct {
-	Number string
+	Number int
 	Suit   rune
 }
 
@@ -17,9 +17,9 @@ type Hand struct {
 	HandRank HandRank
 }
 
-type Hands []Hand
-
 type HandRank int
+
+var PictureToNumber = map[string]int{"J": 11, "Q": 12, "K": 13, "A": 14}
 
 const (
 	HighCard HandRank = iota
@@ -38,7 +38,10 @@ func BestHand(hands []string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(handsSplit)
+	for _, hand := range handsSplit {
+		hand.FindRank()
+	}
+
 	panic("Please implement the BestHand function")
 }
 
@@ -47,8 +50,8 @@ func BestHand(hands []string) ([]string, error) {
 // Otherwise check for straight and/or flush. The four remaining
 // hand types stem from this
 
-func ProcessCards(hands []string) ([][]Card, error) {
-	handsSplit := [][]Card{}
+func ProcessCards(hands []string) ([]Hand, error) {
+	handsSplit := []Hand{}
 	for _, hand := range hands {
 		separateCards := strings.Split(hand, " ")
 		if len(separateCards) != 5 {
@@ -61,20 +64,111 @@ func ProcessCards(hands []string) ([][]Card, error) {
 			if suit != '♡' && suit != '♢' && suit != '♤' && suit != '♧' {
 				return nil, errors.New("no appropriate suit")
 			}
-			number := string(runeCard[:len(runeCard)-1])
-			if number != "J" && number != "Q" && number != "K" && number != "A" {
-				numberInt, err := strconv.Atoi(number)
+			numberString := string(runeCard[:len(runeCard)-1])
+			number := 0
+			if numberString == "J" || numberString == "Q" || numberString == "K" || numberString == "A" {
+				number = PictureToNumber[numberString]
+			} else {
+				numberInt, err := strconv.Atoi(numberString)
 				if err != nil {
 					return nil, err
 				}
 				if numberInt < 2 || numberInt > 10 {
 					return nil, errors.New("card number must be between 2 and 10")
 				}
+				number = numberInt
 			}
 			handSplit = append(handSplit, Card{number, suit})
 		}
-
-		handsSplit = append(handsSplit, handSplit)
+		sort.Slice(handSplit, func(i, j int) bool { return handSplit[i].Number < handSplit[j].Number })
+		handsSplit = append(handsSplit, Hand{Cards: handSplit})
 	}
 	return handsSplit, nil
+}
+
+func (h *Hand) FindRank() {
+	if h.PairExists() {
+		if h.IsFourOfAKind() {
+			h.HandRank = FourOfAKind
+		} else if h.IsFullHouse() {
+			h.HandRank = FullHouse
+		} else if h.IsThreeOfAKind() {
+			h.HandRank = ThreeOfAKind
+		} else if h.IsTwoPair() {
+			h.HandRank = TwoPair
+		} else {
+			h.HandRank = OnePair
+		}
+	} else {
+		if h.IsFlush() && h.IsStraight() {
+			h.HandRank = StraightFlush
+		} else if h.IsFlush() {
+			h.HandRank = Flush
+		} else if h.IsStraight() {
+			h.HandRank = Straight
+		} else {
+			h.HandRank = HighCard
+		}
+	}
+}
+
+func (h *Hand) PairExists() bool {
+	return h.Cards[0].Number == h.Cards[1].Number &&
+		h.Cards[1].Number == h.Cards[2].Number &&
+		h.Cards[2].Number == h.Cards[3].Number &&
+		h.Cards[3].Number == h.Cards[4].Number
+}
+
+func (h *Hand) IsStraight() bool {
+	return (h.Cards[0].Number+1 == h.Cards[1].Number &&
+		h.Cards[1].Number+1 == h.Cards[2].Number &&
+		h.Cards[2].Number+1 == h.Cards[3].Number &&
+		h.Cards[3].Number+1 == h.Cards[4].Number) ||
+		(h.Cards[0].Number+1 == h.Cards[1].Number &&
+			h.Cards[1].Number+1 == h.Cards[2].Number &&
+			h.Cards[2].Number+1 == h.Cards[3].Number &&
+			(h.Cards[4].Number%13)+1 == h.Cards[0].Number)
+}
+
+func (h *Hand) IsFlush() bool {
+	return h.Cards[0].Suit == h.Cards[1].Suit &&
+		h.Cards[1].Suit == h.Cards[2].Suit &&
+		h.Cards[2].Suit == h.Cards[3].Suit &&
+		h.Cards[3].Suit == h.Cards[4].Suit
+}
+
+func (h *Hand) IsFourOfAKind() bool {
+	return (h.Cards[0].Number == h.Cards[1].Number &&
+		h.Cards[1].Number+1 == h.Cards[2].Number &&
+		h.Cards[2].Number+1 == h.Cards[3].Number) ||
+		(h.Cards[1].Number+1 == h.Cards[2].Number &&
+			h.Cards[2].Number+1 == h.Cards[3].Number &&
+			h.Cards[3].Number+1 == h.Cards[4].Number)
+}
+
+func (h *Hand) IsFullHouse() bool {
+	return (h.Cards[0].Number == h.Cards[1].Number &&
+		h.Cards[2].Number == h.Cards[3].Number &&
+		h.Cards[3].Number == h.Cards[4].Number) ||
+		(h.Cards[0].Number == h.Cards[1].Number &&
+			h.Cards[1].Number == h.Cards[2].Number &&
+			h.Cards[3].Number == h.Cards[4].Number)
+}
+
+func (h *Hand) IsThreeOfAKind() bool {
+	return (h.Cards[0].Number == h.Cards[1].Number &&
+		h.Cards[1].Number == h.Cards[2].Number) ||
+		(h.Cards[1].Number == h.Cards[2].Number &&
+			h.Cards[2].Number == h.Cards[3].Number) ||
+		(h.Cards[2].Number == h.Cards[3].Number &&
+			h.Cards[3].Number == h.Cards[4].Number)
+}
+
+func (h *Hand) IsTwoPair() bool {
+	return (h.Cards[0].Number == h.Cards[1].Number &&
+		h.Cards[2].Number == h.Cards[3].Number) ||
+		(h.Cards[0].Number == h.Cards[1].Number &&
+			h.Cards[3].Number == h.Cards[4].Number) ||
+		(h.Cards[1].Number == h.Cards[2].Number &&
+			h.Cards[3].Number == h.Cards[4].Number)
 }
