2d7a917cc181ea499e1930138100423b238b1ad8
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-23 14:45:16 +0000

    Add Beer Song for Rust

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        2 => format!("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
+        1 => format!("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        0 => format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    let mut song = String::new();
+    for i in (end..=start).rev() {
+        if i != start {
+            song = format!("{}\n", song)
+        }
+        song = format!("{}{}", song, verse(i))
+    }
+    song
+}
