76fe318bb63315ced1ee6d425c86d634726853f7
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-03-31 09:53:34 +0100

    Add Two Bucket

--- go/two-bucket/two_bucket.go
+++ go/two-bucket/two_bucket.go
@@ -1,18 +1,65 @@
 package twobucket
 
-import "errors"
+import (
+	"errors"
+)
+
+type Bucket struct {
+	Number   string
+	Holding  int
+	Capacity int
+}
 
 func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
 	if sizeBucketOne <= 0 || sizeBucketTwo <= 0 || goalAmount <= 0 {
 		return "", 0, 0, errors.New("bucket sizes and goal amount must be greater than 0")
+	} else if goalAmount%(HighestCommonFactor(sizeBucketOne, sizeBucketTwo)) != 0 {
+		return "", 0, 0, errors.New("goal amount cannot be reached with the given buckets")
 	}
-	return "", 0, 0, nil
+	if goalAmount == sizeBucketOne && startBucket == "two" && sizeBucketOne != sizeBucketTwo {
+		return "one", 2, sizeBucketTwo, nil
+	} else if goalAmount == sizeBucketTwo && startBucket == "one" && sizeBucketOne != sizeBucketTwo {
+		return "two", 2, sizeBucketOne, nil
+	}
+	firstBucket, secondBucket := Bucket{}, Bucket{}
+	if startBucket == "one" {
+		firstBucket.Number, firstBucket.Capacity, firstBucket.Holding = "one", sizeBucketOne, sizeBucketOne
+		secondBucket.Number, secondBucket.Capacity = "two", sizeBucketTwo
+		secondBucket.Capacity = sizeBucketTwo
+	} else if startBucket == "two" {
+		firstBucket.Number, firstBucket.Capacity, firstBucket.Holding = "two", sizeBucketTwo, sizeBucketTwo
+		secondBucket.Number, secondBucket.Capacity = "one", sizeBucketOne
+	} else {
+		return "", 0, 0, errors.New("invalid start bucket")
+	}
+	actions := 1
+	for {
+		if firstBucket.Holding == goalAmount {
+			return firstBucket.Number, actions, secondBucket.Holding, nil
+		} else if secondBucket.Holding == goalAmount {
+			return secondBucket.Number, actions, firstBucket.Holding, nil
+		}
+		if secondBucket.Holding == secondBucket.Capacity {
+			secondBucket.Holding = 0
+		} else if firstBucket.Holding == 0 {
+			firstBucket.Holding = firstBucket.Capacity
+		} else {
+			var transferAmount int
+			if firstBucket.Holding < secondBucket.Capacity-secondBucket.Holding {
+				transferAmount = firstBucket.Holding
+			} else {
+				transferAmount = secondBucket.Capacity - secondBucket.Holding
+			}
+			firstBucket.Holding -= transferAmount
+			secondBucket.Holding += transferAmount
+		}
+		actions++
+	}
 }
 
-// Fill the first bucket
-// If either bucket has the same size as the required amount, fill if necessary and return
-// Otherwise, pour the full bucket into the other bucket since if you emptied it, you would 
-// have to refill it, and if you filled the other bucket, you would have to empty it
-// Check if either bucket has the required amount, and if so, return
-// Otherwise, either fill the first bucket if it is empty or empty the other bucket if it is full
-// Pour the first bucket into the other bucket and continue
+func HighestCommonFactor(numberOne, numberTwo int) int {
+	for numberTwo != 0 {
+		numberOne, numberTwo = numberTwo, numberOne%numberTwo
+	}
+	return numberOne
+}
