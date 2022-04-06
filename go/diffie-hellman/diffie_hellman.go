95ab7c30fb7b8b86f0ef9953459f347e3baffc8c
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-04-06 19:49:29 +0100

    Update Diffie Hellman

--- go/diffie-hellman/diffie_hellman.go
+++ go/diffie-hellman/diffie_hellman.go
@@ -5,21 +5,16 @@ import (
 	"math/big"
 )
 
-// Diffie-Hellman-Merkle key exchange
-// Private keys should be generated randomly.
-
 func PrivateKey(p *big.Int) *big.Int {
-	var z big.Int
-	key, err := rand.Int(rand.Reader, z.Sub(p, big.NewInt(2)))
+	key, err := rand.Int(rand.Reader, new(big.Int).Sub(p, big.NewInt(2)))
 	if err != nil {
 		panic("something went wrong generating private key")
 	}
-	return z.Add(key, big.NewInt(2))
+	return new(big.Int).Add(key, big.NewInt(2))
 }
 
 func PublicKey(private, p *big.Int, g int64) *big.Int {
-	var z big.Int
-	return z.Exp(big.NewInt(g), private, p)
+	return new(big.Int).Exp(big.NewInt(g), private, p)
 }
 
 func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
@@ -29,6 +24,5 @@ func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
 }
 
 func SecretKey(private1, public2, p *big.Int) *big.Int {
-	var z big.Int
-	return z.Exp(public2, private1, p)
+	return new(big.Int).Exp(public2, private1, p)
 }
