8a3fb64f5ea402d04774d05aad022acc10a709df
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-04-17 00:02:52 +0100

    Add Variable Length Quantity

--- go/variable-length-quantity/variable_length_quantity.go
+++ go/variable-length-quantity/variable_length_quantity.go
@@ -1,12 +1,14 @@
 package variablelengthquantity
 
+import "errors"
+
 func EncodeVarint(input []uint32) []byte {
 	output := []byte{}
 	for _, integer := range input {
 		encoded := []byte{byte(integer % 128)}
 		integer /= 128
 		for integer > 0 {
-			encoded = append([]byte{byte((integer % 128)+128)}, encoded...)
+			encoded = append([]byte{byte((integer % 128) + 128)}, encoded...)
 			integer /= 128
 		}
 		output = append(output, encoded...)
@@ -15,5 +17,23 @@ func EncodeVarint(input []uint32) []byte {
 }
 
 func DecodeVarint(input []byte) ([]uint32, error) {
-	return []uint32{}, nil
+	output := []uint32{}
+	integer := uint32(0)
+	completeSequences := true
+	for _, inputByte := range input {
+		integer <<= 7
+		if inputByte >= 128 {
+			integer += uint32(inputByte - 128)
+			completeSequences = false
+		} else {
+			integer += uint32(inputByte)
+			output = append(output, integer)
+			integer = 0
+			completeSequences = true
+		}
+	}
+	if !completeSequences {
+		return nil, errors.New("incomplete sequence")
+	}
+	return output, nil
 }
