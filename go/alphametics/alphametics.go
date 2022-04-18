fedf916fdf871fd7c3d267b744484d5f66594f63
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-04-19 00:39:14 +0100

    Add Alphametics

--- go/alphametics/alphametics.go
+++ go/alphametics/alphametics.go
@@ -2,67 +2,59 @@ package alphametics
 
 import (
 	"errors"
-	"math"
 	"strings"
 )
 
 func Solve(puzzle string) (map[string]int, error) {
 	separateSides := strings.Split(puzzle, " == ")
 	summands, sum := strings.Split(separateSides[0], " + "), separateSides[1]
-	totalValues := map[string]int{}
+	totalContributions := map[string]int{}
 	isFirstDigit := map[string]bool{}
 	for _, word := range summands {
 		multiplicand := 1
 		for i := len(word) - 1; i >= 0; i-- {
 			letter := string(word[i])
-			if i == 0 && len(word) > 1 {
+			if i == 0 {
 				isFirstDigit[letter] = true
 			}
-			totalValues[letter] += multiplicand
+			totalContributions[letter] += multiplicand
 			multiplicand *= 10
 		}
 	}
 	multiplicand := 1
 	for i := len(sum) - 1; i >= 0; i-- {
 		letter := string(sum[i])
-		if i == 0 && len(sum) > 1 {
+		if i == 0 {
 			isFirstDigit[letter] = true
 		}
-		totalValues[letter] -= multiplicand
+		totalContributions[letter] -= multiplicand
 		multiplicand *= 10
 	}
-
 	letters := []string{}
-	for key := range totalValues {
+	for key := range totalContributions {
 		letters = append(letters, key)
 	}
 	solutions := []map[string]int{}
-	for i := 0; i < int(math.Pow(10, float64(len(letters)))); i++ {
-		numberUsed := map[int]bool{}
+	for i := 0; i < Factorial(10)/Factorial(10-len(letters)); i++ {
 		letterValues := map[string]int{}
-		numberToTest := i
-		duplicateNumber := false
+		iCopy := i
 		firstDigitZero := false
-		for _, letter := range letters {
-			numberToTry := numberToTest % 10
-			if numberUsed[numberToTry] {
-				duplicateNumber = true
-				break
-			}
+		numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+		sum := 0
+		for index, letter := range letters {
+			numbersIndex := iCopy % (10-index)
+			numberToTry := numbers[numbersIndex]
 			if numberToTry == 0 && isFirstDigit[letter] {
 				firstDigitZero = true
 				break
 			}
-			numberUsed[numberToTry] = true
 			letterValues[letter] = numberToTry
-			numberToTest /= 10
+			sum += totalContributions[letter] * letterValues[letter]
+			numbers = append(numbers[:numbersIndex], numbers[numbersIndex+1:]...)
+			iCopy /= (10-index)
 		}
-		if duplicateNumber || firstDigitZero {
+		if firstDigitZero {
 			continue
-		}
-		sum := 0
-		for _, letter := range letters {
-			sum += totalValues[letter] * letterValues[letter]
 		}
 		if sum == 0 {
 			solutions = append(solutions, letterValues)
@@ -73,4 +65,11 @@ func Solve(puzzle string) (map[string]int, error) {
 	} else {
 		return solutions[0], nil
 	}
+}
+
+func Factorial(input int) int {
+	if input == 1 || input == 0 {
+		return 1
+	}
+	return input * Factorial(input-1)
 }
