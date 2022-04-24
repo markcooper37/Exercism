1a0b5b43ddcd897cd561d56a847f36d2568df5de
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-04-25 00:05:38 +0100

    Add Rail Fence Cipher

--- go/rail-fence-cipher/rail_fence_cipher.go
+++ go/rail-fence-cipher/rail_fence_cipher.go
@@ -5,26 +5,48 @@ import "strings"
 func Encode(message string, rails int) string {
 	pieces := make([]string, rails)
 	line := 0
-	inc := true
+	step := 1
 	for _, character := range message {
 		if character == ' ' {
 			continue
 		}
 		pieces[line] += string(character)
-		if inc {
-			line++
-		} else {
-			line--
-		}
-		if line == rails-1 {
-			inc = false
-		} else if line == 0 {
-			inc = true
+		line += step
+		if line == 0 || line == rails-1 {
+			step *= -1
 		}
 	}
 	return strings.Join(pieces, "")
 }
 
 func Decode(message string, rails int) string {
-	return ""
+	lengths := make([]int, rails)
+	line := 0
+	step := 1
+	for i := 0; i < len(message); i++ {
+		lengths[line]++
+		line += step
+		if line == 0 || line == rails-1 {
+			step *= -1
+		}
+	}
+	pieces := make([]string, rails)
+	start := 0
+	for index, length := range lengths {
+		pieces[index] = message[start:start+length]
+		start += length
+	}
+	indices := make([]int, rails)
+	decoded := ""
+	line = 0
+	step = 1
+	for i := 0; i < len(message); i++ {
+		decoded += string(pieces[line][indices[line]])
+		indices[line]++
+		line += step
+		if line == 0 || line == rails-1 {
+			step *= -1
+		}
+	}
+	return decoded
 }
