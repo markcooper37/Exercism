4960064b71b55b40a8195e2a67840d1d83a1916a
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-04-27 16:46:03 +0100

    Add Bank Account

---
+++ go/bank-account/bank_account.go
@@ -0,0 +1,51 @@
+package account
+
+import "sync"
+
+// Define the Account type here.
+type Account struct {
+	mu    sync.Mutex
+	money int64
+	open bool
+}
+
+func Open(amount int64) *Account {
+	if amount < 0 {
+		return nil
+	}
+	return &Account{money: amount, open: true}
+}
+
+func (a *Account) Balance() (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.open {
+		return a.money, true
+	} else {
+		return 0, false
+	}
+}
+
+func (a *Account) Deposit(amount int64) (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if !a.open || a.money + amount < 0 {
+		return 0, false
+	} else {
+		a.money += amount
+		return a.money, true
+	}
+}
+
+func (a *Account) Close() (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.open {
+		a.open = false
+		money := a.money
+		a.money = 0
+		return money, true
+	} else {
+		return 0, false
+	}
+}
