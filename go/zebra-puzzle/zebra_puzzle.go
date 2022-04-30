8154ceb9ac2cd77291250e9e7cbb3f769333293a
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-04-30 23:13:41 +0100

    Initial Zebra commit

---
+++ go/zebra-puzzle/zebra_puzzle.go
@@ -0,0 +1,16 @@
+package zebra
+
+type Solution struct {
+	DrinksWater string
+	OwnsZebra   string
+}
+
+func SolvePuzzle() Solution {
+	panic("Please implement the SolvePuzzle function")
+}
+
+// Englishman, Spaniard, Ukranian, Norwegian, Japanese
+// red, green, yellow, blue, ivory
+// dog, snails, fox, horse, zebra
+// coffee, tea, milk, orange juice, water
+// Old Gold, Kool, Chesterfield, Lucky Strike, Parliament
