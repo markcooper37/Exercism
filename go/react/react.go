049c3adca41eadf63ca06ab65b781d422bfe5f62
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-05-05 13:20:30 +0100

    Improve React

--- go/react/react.go
+++ go/react/react.go
@@ -1,29 +1,21 @@
 package react
 
 type cell struct {
-	value     int
-	reactor   *reactor
-	dep1      *Cell
-	dep2      *Cell
-	func1     func(int) int
-	func2     func(int, int) int
-	callbacks []*func(int)
+	value        int
+	reactor      *reactor
+	computeValue func() int
+	callbacks    map[int]func(int)
 }
 type reactor struct {
 	computeCells []*cell
 }
 type canceler struct {
-	cell     *cell
-	callback *func(int)
+	cell *cell
+	id   int
 }
 
 func (c *canceler) Cancel() {
-	for index, callback := range c.cell.callbacks {
-		if callback == c.callback {
-			c.cell.callbacks = append(c.cell.callbacks[:index], c.cell.callbacks[index+1:]...)
-			return
-		}
-	}
+	delete(c.cell.callbacks, c.id)
 }
 
 func (c *cell) Value() int {
@@ -33,24 +25,23 @@ func (c *cell) Value() int {
 func (c *cell) SetValue(value int) {
 	c.value = value
 	for _, computeCell := range c.reactor.computeCells {
-		var newValue int
-		if computeCell.func1 != nil {
-			newValue = computeCell.func1((*(computeCell.dep1)).Value())
-		} else {
-			newValue = computeCell.func2((*(computeCell.dep1)).Value(), (*(computeCell.dep2)).Value())
-		}
-		if newValue != computeCell.value {
+		if newValue := computeCell.computeValue(); newValue != computeCell.value {
 			computeCell.value = newValue
 			for _, callback := range computeCell.callbacks {
-				(*callback)(computeCell.value)
+				callback(computeCell.value)
 			}
 		}
 	}
 }
 
 func (c *cell) AddCallback(callback func(int)) Canceler {
-	c.callbacks = append(c.callbacks, &callback)
-	return &canceler{cell: c, callback: &callback}
+	for i := 0; i <= len(c.callbacks); i++ {
+		if _, ok := c.callbacks[i]; !ok {
+			c.callbacks[i] = callback
+			return &canceler{cell: c, id: i}
+		}
+	}
+	return nil
 }
 
 func New() Reactor {
@@ -58,19 +49,19 @@ func New() Reactor {
 }
 
 func (r *reactor) CreateInput(initial int) InputCell {
-	cell := cell{value: initial, reactor: r}
-	return &cell
-
+	return &cell{value: initial, reactor: r}
 }
 
 func (r *reactor) CreateCompute1(dep Cell, compute func(int) int) ComputeCell {
-	cell := cell{value: compute(dep.Value()), reactor: r, dep1: &dep, func1: compute}
+	computeValue := func() int { return compute(dep.Value()) }
+	cell := cell{value: computeValue(), computeValue: computeValue, reactor: r, callbacks: map[int]func(int){}}
 	r.computeCells = append(r.computeCells, &cell)
 	return &cell
 }
 
 func (r *reactor) CreateCompute2(dep1, dep2 Cell, compute func(int, int) int) ComputeCell {
-	cell := cell{value: compute(dep1.Value(), dep2.Value()), reactor: r, dep1: &dep1, dep2: &dep2, func2: compute}
+	computeValue := func() int { return compute(dep1.Value(), dep2.Value()) }
+	cell := cell{value: computeValue(), computeValue: computeValue, reactor: r, callbacks: map[int]func(int){}}
 	r.computeCells = append(r.computeCells, &cell)
 	return &cell
 }
