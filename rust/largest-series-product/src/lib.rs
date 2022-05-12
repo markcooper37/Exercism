7086d2067e565694b1791aebaa06025568163f8b
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-05-12 11:27:07 +0100

    Add Largest Series Product for Rust

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -5,14 +5,25 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    for character in string_digits.chars() {
+        if !character.is_ascii_digit() {
+            return Err(Error::InvalidDigit(character));
+        }
+    }
     if span > string_digits.len() {
         Err(Error::SpanTooLong)
     } else {
-        for character in string_digits.chars() {
-            if !character.is_ascii_digit() {
-                return Err(Error::InvalidDigit(character));
-            }
-        } 
-        Ok(0)
-    } 
+        if span == 0 {
+            return Ok(1);
+        }
+        let mut something = string_digits
+            .bytes()
+            .map(|c| (c - 48) as u64)
+            .collect::<Vec<u64>>()
+            .windows(span)
+            .map(|x| x.iter().product())
+            .collect::<Vec<u64>>();
+        something.sort_by(|a, b| b.cmp(a));
+        Ok(something[0])
+    }
 }
