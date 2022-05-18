757a2ebc21fda54ab9a93cbe1ff0b31c21044d37
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-05-18 11:46:40 +0100

    Add Isbn Verifier for Rust

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -1,12 +1,22 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let mut isbn = isbn.chars().filter(|c| c.is_alphanumeric()).collect::<String>();
+    let isbn = isbn
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect::<String>();
     if isbn.len() != 10 {
         return false;
     }
-    isbn = isbn.chars().enumerate().filter(|c| c.1.is_digit(10) || c.0 == 9 && c.1 == 'X').map(|c| c.1).collect::<String>();
-    if isbn.len() != 10 {
-        return false;
+    for c in isbn.chars().enumerate() {
+        if !c.1.is_digit(10) && (c.0 < 9 || c.1 != 'X') {
+            return false;
+        }
     }
-    isbn.chars().map(|c| c.to_digit(10).unwrap_or(10)).enumerate().map(|c| c.1 * (10 - c.0 as u32)).sum::<u32>() % 11 == 0
+    isbn.chars()
+        .map(|c| c.to_digit(10).unwrap_or(10))
+        .enumerate()
+        .map(|c| c.1 * (10 - c.0 as u32))
+        .sum::<u32>()
+        % 11
+        == 0
 }
