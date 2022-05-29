cac068e4d63b59a8a1495bbcb0fbaf3a9810fdcb
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-05-30 00:04:41 +0100

    Add Anagram for Rust

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,11 +1,20 @@
-use std::{collections::HashSet, ops::RangeBounds};
+use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let word: Vec<char> = word.to_lowercase().chars().collect();
-    let possible_anagrams = possible_anagrams
-        .iter()
-        .map(|w| w.to_lowercase().chars().collect::<Vec<char>>()).filter(|w| *w != word);
-    word.sort_unstable();
-    possible_anagrams.filter(|w| w.contains(word) && word.contains(w));
-    
+    let mut anagrams: HashSet<&str> = HashSet::new();
+    let word = word.to_lowercase();
+    for anagram in possible_anagrams {
+        let anagram_lower = anagram.to_lowercase();
+        if anagram_lower == word {
+            continue;
+        }
+        let mut word_chars: Vec<char> = word.chars().collect();
+        word_chars.sort_unstable();
+        let mut anagram_chars: Vec<char> = anagram_lower.chars().collect();
+        anagram_chars.sort_unstable();
+        if word_chars == anagram_chars {
+            anagrams.insert(anagram);
+        }
+    }
+    anagrams
 }
