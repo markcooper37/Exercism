c31f8d9ce6526f4a426a61301d708c6de60363b0
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-06-06 11:56:12 +0100

    Add improved Sublist for Rust

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -9,32 +7,25 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    match _first_list.len().cmp(&_second_list.len()) {
-        Ordering::Equal => {
-            match _first_list == _second_list {
-                true => Comparison::Equal,
-                false => Comparison::Unequal
-            }
-        }
-        Ordering::Less => {
-            match is_first_sublist(_first_list, _second_list) {
-                true => Comparison::Sublist,
-                false => Comparison::Unequal
-            }
-        }
-        Ordering::Greater => {
-            match is_first_sublist(_second_list, _first_list) {
-                true => Comparison::Superlist,
-                false => Comparison::Unequal
-            }
-        }
+    if _first_list == _second_list {
+        Comparison::Equal
+    } else if _first_list.len() < _second_list.len() && is_first_sublist(_first_list, _second_list)
+    {
+        Comparison::Sublist
+    } else if _first_list.len() > _second_list.len() && is_first_sublist(_second_list, _first_list)
+    {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
     }
 }
 
 pub fn is_first_sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> bool {
-    (0..=_second_list.len() - _first_list.len()).any(|i| _second_list
-        .iter()
-        .skip(i)
-        .zip(_first_list.iter())
-        .all(|x| x.0 == x.1))
+    (0..=_second_list.len() - _first_list.len()).any(|i| {
+        _second_list
+            .iter()
+            .skip(i)
+            .zip(_first_list.iter())
+            .all(|x| x.0 == x.1)
+    })
 }
