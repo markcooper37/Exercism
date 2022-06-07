b8301a1faf29689e3be5a21be55f708ab742b0ce
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-06-07 12:48:56 +0100

    Remove regex in Word Count for Rust

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,13 +1,12 @@
-use regex::Regex;
 use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    Regex::new("([a-z]+'[a-z]+)|([a-z]+)|([0-9]+)")
-        .unwrap()
-        .find_iter(&(words.to_lowercase()))
-        .fold(HashMap::<String, u32>::new(), |mut m, x| {
-            *m.entry(x.as_str().to_string()).or_default() += 1;
+    words
+        .split(|c: char| !c.is_alphanumeric() && c != '\'')
+        .filter(|s| !s.is_empty())
+        .fold(HashMap::new(), |mut m, x| {
+            *m.entry(x.trim_matches('\'').to_lowercase()).or_default() += 1;
             m
         })
 }
