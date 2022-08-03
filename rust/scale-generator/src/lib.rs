29886a698c2ced04c223cb2f0114e72249fcae78
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-08-03 16:15:15 +0100

    Improve Scale Generator for Rust

--- rust/scale-generator/src/lib.rs
+++ rust/scale-generator/src/lib.rs
@@ -1,40 +1,13 @@
 #[derive(Debug)]
 pub enum Error {
     InvalidTonic,
-    InvalidIntervalLetter,
-    IntervalsTooLong,
-    IntervalsTooShort,
+    InvalidInterval,
 }
 
 pub struct Scale(Vec<String>);
 
 impl Scale {
     pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
-        let chromatic: Vec<String>;
-        match Scale::chromatic(tonic) {
-            Ok(scale) => chromatic = scale.0,
-            Err(x) => return Err(x),
-        }
-        let (mut scale, mut pos) = (vec![chromatic[0].clone()], 0);
-        for character in intervals.chars() {
-            match character {
-                'm' => pos += 1,
-                'M' => pos += 2,
-                'A' => pos += 3,
-                _ => return Err(Error::InvalidIntervalLetter),
-            }
-            if pos > 12 {
-                return Err(Error::IntervalsTooLong);
-            }
-            scale.push(chromatic[pos].clone());
-        }
-        match pos {
-            12 => Ok(Scale(scale)),
-            _ => Err(Error::IntervalsTooShort)
-        }
-    }
-
-    pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
         let (sharp_scale, flat_scale) = (
             [
                 "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
@@ -43,26 +16,33 @@ impl Scale {
                 "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
             ],
         );
-        let scale: [&str; 12];
-        match tonic {
+        let scale = match tonic {
             "C" | "G" | "D" | "A" | "E" | "B" | "F#" | "a" | "e" | "b" | "f#" | "c#" | "g#"
-            | "d#" => scale = sharp_scale,
+            | "d#" => sharp_scale,
             "F" | "Bb" | "Eb" | "Ab" | "Db" | "Gb" | "d" | "g" | "c" | "f" | "bb" | "eb" => {
-                scale = flat_scale
+                flat_scale
             }
             _ => return Err(Error::InvalidTonic),
+        };
+        let mut pos = scale
+            .iter()
+            .position(|&c| c.to_lowercase() == tonic.to_lowercase())
+            .unwrap();
+        let mut new_scale = vec![scale[pos].to_string()];
+        for interval in intervals.chars() {
+            pos += match interval {
+                'm' => 1,
+                'M' => 2,
+                'A' => 3,
+                _ => return Err(Error::InvalidInterval),
+            };
+            new_scale.push(scale[pos % 12].to_string())
         }
-        let mut first_position = 0;
-        for (index, scale_tonic) in scale.iter().enumerate() {
-            if tonic.to_lowercase() == scale_tonic.to_lowercase() {
-                first_position = index;
-                break;
-            }
-        }
-        let scale = scale.iter().map(|c| c.to_string()).collect::<Vec<String>>();
-        let mut chromatic = scale[first_position..].to_vec();
-        chromatic.append(&mut scale[..=first_position].to_vec());
-        Ok(Scale(chromatic))
+        Ok(Self(new_scale))
+    }
+
+    pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
+        Self::new(tonic, "mmmmmmmmmmmm")
     }
 
     pub fn enumerate(&self) -> Vec<String> {
