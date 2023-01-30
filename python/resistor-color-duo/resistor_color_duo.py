3b8b110302488180c398eda00b9742c05c9e6083
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-01-30 14:02:39 +0000

    Add Resistor Color Duo for Python

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,3 @@
+def value(colors):
+    values = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+    return values.index(colors[0]) * 10 + values.index(colors[1])
