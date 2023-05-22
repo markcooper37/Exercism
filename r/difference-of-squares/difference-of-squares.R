9c4658c5d867d7b916e5f42638e7a866fe7180ab
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-05-22 12:29:48 +0100

    Add Difference of Squares for R

---
+++ r/difference-of-squares/difference-of-squares.R
@@ -0,0 +1,6 @@
+# this is a stub function that takes a natural_number
+# and should return the difference-of-squares as described
+# in the README.md
+difference_of_squares <- function(natural_number) {
+  return(natural_number * (natural_number^2 - 1) * (3 * natural_number + 2) / 12)
+}
