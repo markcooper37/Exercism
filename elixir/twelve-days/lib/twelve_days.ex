bb448128ee4f32a8517f806d2c36665f9ce2d3e2
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-06-27 10:48:25 +0100

    Add Twelve Days for Elixir

---
+++ elixir/twelve-days/lib/twelve_days.ex
@@ -0,0 +1,38 @@
+defmodule TwelveDays do
+  @days %{1 => "first", 2 => "second", 3 => "third", 4 => "fourth", 5 => "fifth", 6 => "sixth", 7 => "seventh", 8 => "eighth", 9 => "ninth", 10 => "tenth", 11 => "eleventh", 12 => "twelfth"}
+
+  @gifts %{1 => "a Partridge in a Pear Tree.", 2 => "two Turtle Doves, and ", 3 => "three French Hens, ", 4 => "four Calling Birds, ", 5 => "five Gold Rings, ", 6 => "six Geese-a-Laying, ", 7 => "seven Swans-a-Swimming, ", 8 => "eight Maids-a-Milking, ", 9 => "nine Ladies Dancing, ", 10 => "ten Lords-a-Leaping, ", 11 => "eleven Pipers Piping, ", 12 => "twelve Drummers Drumming, "}
+
+  @doc """
+  Given a `number`, return the song's verse for that specific day, including
+  all gifts for previous days in the same line.
+  """
+  @spec verse(number :: integer) :: String.t()
+  def verse(number) do
+    "On the #{@days[number]} day of Christmas my true love gave to me: #{add_gifts(number)}"
+  end
+
+  defp add_gifts(1), do: @gifts[1]
+  defp add_gifts(number) do
+    @gifts[number] <> add_gifts(number - 1)
+  end
+
+  @doc """
+  Given a `starting_verse` and an `ending_verse`, return the verses for each
+  included day, one per line.
+  """
+  @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
+  def verses(starting_verse, ending_verse) do
+    starting_verse..ending_verse
+    |> Enum.map(&verse/1)
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Sing all 12 verses, in order, one verse per line.
+  """
+  @spec sing() :: String.t()
+  def sing do
+    verses(1, 12)
+  end
+end
