d9c16dfc3f4b745f554c04dc1fb1dce6c268dbc2
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-07-09 19:54:34 +0100

    Add Darts for C++

---
+++ cpp/darts/darts.cpp
@@ -0,0 +1,13 @@
+#include "darts.h"
+#include <cmath>
+#include <iostream>
+
+namespace darts {
+    int score(double x, double y) {
+        double distance = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+        if (distance <= 1) return 10;
+        else if (distance <= 5) return 5;
+        else if (distance <= 10) return 1;
+        else return 0;
+    }
+} // namespace darts
