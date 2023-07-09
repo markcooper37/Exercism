922915c7ac34a9de19dfc0fde34de050f1dabdf5
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-07-09 18:47:58 +0100

    Add Two Fer for C++

---
+++ cpp/two-fer/two_fer.cpp
@@ -0,0 +1,12 @@
+#include "two_fer.h"
+#include <string>
+
+namespace two_fer
+{
+
+    std::string two_fer(std::string name) {
+        return "One for " + name + ", one for me.";
+    }
+
+} // namespace two_fer
+
