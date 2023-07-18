f67a50699e2b30b4c54fd8eea376039a50a440b2
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-07-18 23:20:29 +0100

    Add Dancing Dots for Elixir

---
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -0,0 +1,47 @@
+defmodule DancingDots.Animation do
+  @type dot :: DancingDots.Dot.t()
+  @type opts :: keyword
+  @type error :: any
+  @type frame_number :: pos_integer
+
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: dot
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
+end
+
+defmodule DancingDots.Flicker do
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, _) do
+    cond do
+      rem(frame_number, 4) == 0 -> %{dot | opacity: dot.opacity/2}
+      true -> dot
+    end
+  end
+end
+
+defmodule DancingDots.Zoom do
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init(opts) do
+    velocity = opts[:velocity]
+    cond do
+      is_number(opts[:velocity]) -> {:ok, opts}
+      true -> {:error, "The :velocity option is required, and its value must be a number. Got: #{inspect(velocity)}"}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    %{dot | radius: dot.radius + (frame_number - 1) * opts[:velocity]}
+  end
+end
