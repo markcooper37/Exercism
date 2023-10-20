118cf59b238277ead45016d3208e1685ffe25b7e
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-10-20 20:53:07 +0100

    Add Elon's Toy Car in Java

---
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -0,0 +1,28 @@
+public class ElonsToyCar {
+
+    private int distance = 0;
+    private int battery = 100;
+    
+    public static ElonsToyCar buy() {
+        return new ElonsToyCar();
+    }
+
+    public String distanceDisplay() {
+        return "Driven " + this.distance + " meters";
+    }
+
+    public String batteryDisplay() {
+        if (this.battery > 0) {
+            return "Battery at " + this.battery + "%";
+        } else {
+            return "Battery empty";
+        }
+    }
+
+    public void drive() {
+        if (this.battery > 0) {
+            this.distance += 20;
+            this.battery -= 1;
+        }
+    }
+}
