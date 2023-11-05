f4c2f11da9295a8e04ff63887ca85bb14408d31a
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-11-05 00:20:56 +0000

    Add Space Age in Prolog

---
+++ prolog/space-age/space_age.pl
@@ -0,0 +1,8 @@
+space_age("Mercury", AgeSec, Years) :- Years is AgeSec / 0.2408467 / 31557600.
+space_age("Venus", AgeSec, Years) :- Years is AgeSec / 0.61519726 / 31557600.
+space_age("Earth", AgeSec, Years) :- Years is AgeSec / 31557600.
+space_age("Mars", AgeSec, Years) :- Years is AgeSec / 1.8808158 / 31557600.
+space_age("Jupiter", AgeSec, Years) :- Years is AgeSec / 11.862615 / 31557600.
+space_age("Saturn", AgeSec, Years) :- Years is AgeSec / 29.447498 / 31557600.
+space_age("Uranus", AgeSec, Years) :- Years is AgeSec / 84.016846 / 31557600.
+space_age("Neptune", AgeSec, Years) :- Years is AgeSec / 164.79132 / 31557600.
