580f35b188032c09edd5d96eae4699d4d91b2cbc
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-12-01 21:40:23 +0000

    Add Resistor Color in Groovy

---
+++ groovy/resistor-color/src/main/groovy/ResistorColor.groovy
@@ -0,0 +1,9 @@
+class ResistorColor {
+
+    static colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+    static colorCode(color) {
+        return colors.indexOf(color)
+    }
+
+}
