221ef5d5d70e31721a21a4beb2166b80d80ced7a
Mark Cooper <cooperm1700@hotmail.co.uk>
2023-12-03 17:40:54 +0000

    Add Space Age in Groovy

---
+++ groovy/space-age/src/main/groovy/SpaceAge.groovy
@@ -0,0 +1,8 @@
+class SpaceAge {
+
+    def static periods = ["Mercury": 0.2408467, "Venus": 0.61519726, "Earth": 1.0, "Mars": 1.8808158, "Jupiter": 11.862615, "Saturn": 29.447498, "Uranus": 84.016846, "Neptune": 164.79132]
+
+    static double age(String planet, int seconds) {
+        return (seconds / 31557600.0 / periods[planet]).round(2)
+    }
+}
